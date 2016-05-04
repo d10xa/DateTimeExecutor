7c5ced9d37b446846669312548810ad79f28d3ff
d10xa <d10xa@mail.ru>
2016-05-04 10:33:59 +0300

    Add test for overcrowded queue order

--- src/test/java/ru/d10xa/concurrent/SingleThreadDateTimeExecutorTest.java
+++ src/test/java/ru/d10xa/concurrent/SingleThreadDateTimeExecutorTest.java
@@ -44,6 +44,41 @@ public class SingleThreadDateTimeExecutorTest {
         }
     }
 
+    @Test
+    public void queue_overflow_order() throws Exception {
+
+        DateTimeExecutor executor = new SingleThreadDateTimeExecutor(500);
+        ArrayList<LocalDateTime> list = new ArrayList<>();
+        LocalDateTime now = now();
+
+        executor.schedule(now(), () -> {
+            Thread.sleep(1000);
+            return 9;
+        });
+
+        IntStream.range(0, 1000)
+                .parallel()
+                .forEach(i -> {
+                    LocalDateTime randomDateTime = randomLocalDateTime(now);
+                    executor.schedule(randomDateTime, () -> {
+                        list.add(randomDateTime);
+                        return 42;
+                    });
+                });
+
+        executor.shutdown();
+
+        Assert.assertTrue(executor.awaitTermination(10, TimeUnit.SECONDS));
+
+        Assert.assertEquals(1000, list.size());
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            LocalDateTime a = list.get(i);
+            LocalDateTime b = list.get(i + 1);
+            Assert.assertTrue(a.isBefore(b) || a.isEqual(b));
+        }
+    }
+
     private static LocalDateTime randomLocalDateTime(LocalDateTime now) {
         return now.plus(ThreadLocalRandom.current().nextInt(50, 550) * 10, ChronoUnit.MILLIS);
     }
